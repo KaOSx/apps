e3bc25056e9cdce694e2ee7e1bf0b5917985888b
demmm <anke62@gmail.com>
2016-05-10 23:15:03 +0000

    don't show license page if nvidia is not in use

--- calamares/launch-calamares.sh
+++ calamares/launch-calamares.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+if [ ! -f /var/log/nvidia ] || [ ! -f /var/log/nvidia-304xx ]; then
+    sudo sed -i -e 's|- license|#- license|' /usr/share/calamares/settings.conf
+fi
+
 sudo /usr/bin/calamares -d > installation.log
