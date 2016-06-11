618c163a8c23c5c609bc77c65c928a243bbcb7f4
demmm <anke62@gmail.com>
2016-06-11 17:12:54 +0000

    or statement is not correct for cala .sh and statement needed to have any nvidia option install

--- calamares/launch-calamares.sh
+++ calamares/launch-calamares.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -f /var/log/nvidia ] || [ ! -f /var/log/nvidia-340xx ]; then
+if [ ! -f /var/log/nvidia ] && [ ! -f /var/log/nvidia-340xx ]; then
     sudo sed -i -e 's|- license|#- license|' /usr/share/calamares/settings.conf
 fi
 
