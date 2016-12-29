974b792874ccdc7af91d2d86624700d47eb22a20
demmm <anke62@gmail.com>
2016-12-29 21:10:36 +0000

    use pkexec for checkiso don't fail silently when no USB is connected isowriter 1.1.0, start adding verify option

--- kde-common/checkiso.sh
+++ kde-common/checkiso.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-kdialog --passivepopup "Verifying......" 20
+kdialog --passivepopup "Verifying the integrity of the USB in comparison to the downloaded ISO, this can take a few minutes...... " 15
 
 CheckedFile=${1}
 
-UsbDevice=$(blkid | grep iso9660 | head -c 8)
+UsbDevice=$(pkexec /sbin/blkid | grep iso9660 | head -c 8)
 
-md5=$(md5sum ${CheckedFile} | head -c 32)
+if [ ! ${UsbDevice} ]; then
 
-bites=$(wc -c ${CheckedFile} | awk '{print $1;}')
+    kdialog --msgbox "No Flash USB detected"
+    
+else
 
-usb=$(head -c ${bites} ${UsbDevice} | md5sum | head -c 32)
+    md5=$(md5sum ${CheckedFile} | head -c 32)
 
-if [ ${md5} == ${usb} ]; then
-    kdialog --msgbox "The downloaded ISO file and USB write match"
+    bites=$(wc -c ${CheckedFile} | awk '{print $1;}')
+
+    usb=$(pkexec /usr/bin/head -c ${bites} ${UsbDevice} | md5sum | head -c 32)
+    
+    if [ ${md5} == ${usb} ]; then
+    
+        kdialog --msgbox "The downloaded ISO file and USB write match"
+        
+    fi
+
+    if [ ${md5} != ${usb} ]; then
+    
+        kdialog --error "ISO files do not match, do not use this USB for install media. Or make sure only one USB flash device is connected"
+        
+    fi
+    
 fi
 
-if [ ${md5} != ${usb} ]; then
-    kdialog --error "ISO files do not match, do not use this USB for install media"
-fi
 
