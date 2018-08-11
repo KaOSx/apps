d2e69f3d3f86d17e6a6ce0ca9686a8449cdc3d59
demmm <anke62@gmail.com>
2018-08-11 13:53:54 +0000

    protobuf 3.6.1 rebuilds apps bitcoin, clementine, marble, mixx, mumble
    calibre 3.19.0
    firefox rebuild without system hunspell, starting with ff 61, system hunspell no longer supported
    
    plasma packages remove added rm locale dir from 2 years ago, no longer appropiate now that plasma-l10n is no longer in use,
    done for kscreen, ksysguard, libksysguard, khotkeys, kinfocenter, kmenuedit, systemsettings
    
    masterpdfeditor 5.1.12
    sirikali 1.3.5

--- firefox/vendor.js
+++ firefox/vendor.js
@@ -4,6 +4,6 @@ pref("intl.locale.requested", "");
 // Disable default browser checking.
 pref("browser.shell.checkDefaultBrowser", false);
 
-// Don't disable our bundled extensions in the application directory
+// Don't disable bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 pref("extensions.shownSelectionUI", true);
