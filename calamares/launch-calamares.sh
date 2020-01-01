734cb6f56bab9906e1d23c7873f7bc85e5b56516
demmm <anke62@gmail.com>
2020-01-01 11:10:47 +0000

    calamares update release notes move to 2020 build, add prime to launcher script gpsd 3.20 kinfocenter move to 2020 kshutdown 5.2 latte-dock 0.9.6 & 7 telegram-desktop 1.9.3

--- calamares/launch-calamares.sh
+++ calamares/launch-calamares.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -f /var/log/nvidia ] && [ ! -f /var/log/nvidia-340xx ] && [ ! -f /var/log/nvidia-390xx ]; then
+if [ ! -f /var/log/nvidia ] && [ ! -f /var/log/nvidia-prime ] && [ ! -f /var/log/nvidia-340xx ] && [ ! -f /var/log/nvidia-390xx ]; then
     sudo sed -i -e 's|- license|#- license|' /usr/share/calamares/settings.conf
 fi
 
