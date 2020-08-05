cd6c43a67312c40b1bc7258fd128293b1610d55b
demmm <anke62@gmail.com>
2020-08-05 10:16:07 +0000

    lilypond 2.21.4 blender 2.83.4 add nvidia-prime to calamares launcher, new section for localeq if no internet, Map.qml/Offline.qml not working, if Map.qml loads without internet causes calamares crashes libtorrent-rasterbar 1.2.8

--- calamares/launch-calamares.sh
+++ calamares/launch-calamares.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-if [ ! -f /var/log/nvidia ] && [ ! -f /var/log/nvidia-prime ] && [ ! -f /var/log/nvidia-340xx ] && [ ! -f /var/log/nvidia-390xx ]; then
+if [ ! -f /var/log/nvidia ] && [ ! -f /var/log/nvidia-340xx ] \
+    && [ ! -f /var/log/nvidia-390xx ] && [ ! -f /var/log/nvidia-prime ]; then
     sudo sed -i -e 's|- license|#- license|' /usr/share/calamares/settings.conf
+fi
+
+if ! ping -c 1 "8.8.8.8" > /dev/null; then
+    sudo sed -i -e 's|- localeq|#- localeq|' /usr/share/calamares/settings.conf
 fi
 
 sudo /usr/bin/calamares -d > installation.log
