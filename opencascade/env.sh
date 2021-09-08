05525c16c1a7a4495508851af3e13952174872e2
demmm <anke62@gmail.com>
2021-09-08 20:29:31 +0000

    kcp 1.0.6 re-adding med, vtk, opencascade med 4.1.0, patch needed, used from Gentoo opencascade 7.5.3, also patches used from gentoo, redone insall/export scripts, just use one in /etc/profile.d vtk 9.0.3, no cmake call found yet that works to use system libs install insctruction state -DVTK_USE_EXTERNAL, build output shows as ignored no qt5 plugins included in this build

--- opencascade/env.sh
+++ opencascade/env.sh
@@ -1,22 +1,22 @@
 #!/bin/sh -f
 
-export PATH="$PATH:$CASROOT/bin"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$CASROOT/lib"
+export CASROOT=/usr
 
 export CSF_LANGUAGE=us
 export MMGT_CLEAR=1
 export CSF_EXCEPTION_PROMPT=1
 
-export CSF_SHMessage="$CASROOT"/src/SHMessage
-export CSF_MDTVTexturesDirectory="$CASROOT"/src/Textures
-export CSF_XSMessage="$CASROOT"/src/XSMessage
-export CSF_StandardDefaults="$CASROOT"/src/StdResource
-export CSF_PluginDefaults="$CASROOT"/src/StdResource
-export CSF_XCAFDefaults="$CASROOT"/src/StdResource
-export CSF_StandardLiteDefaults="$CASROOT"/src/StdResource
-export CSF_UnitsLexicon="$CASROOT"/src/UnitsAPI/Lexi_Expr.dat
-export CSF_UnitsDefinition="$CASROOT"/src/UnitsAPI/Units.dat
-export CSF_IGESDefaults="$CASROOT"/src/XSTEPResource
-export CSF_STEPDefaults="$CASROOT"/src/XSTEPResource
-export CSF_XmlOcafResource="$CASROOT"/src/XmlOcafResource
-export CSF_GraphicShr="$CASROOT"/lib/libTKOpenGl.so
+export CSF_SHMessage="$CASROOT"/share/opencascade/resources/SHMessage
+export CSF_MIGRATION_TYPES="$CASROOT"/share/opencascade/resources/StdResource/MigrationSheet.txt
+export CSF_MDTVTexturesDirectory="$CASROOT"/share/opencascade/resources/Textures
+export CSF_XSMessage="$CASROOT"/share/opencascade/resources/XSMessage
+export CSF_StandardDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_PluginDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_XCAFDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_StandardLiteDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_UnitsLexicon="$CASROOT"/share/opencascade/resources/UnitsAPI/Lexi_Expr.dat
+export CSF_UnitsDefinition="$CASROOT"/share/opencascade/resources/UnitsAPI/Units.dat
+export CSF_IGESDefaults="$CASROOT"/share/opencascade/resources/XSTEPResource
+export CSF_STEPDefaults="$CASROOT"/share/opencascade/resources/XSTEPResource
+export CSF_XmlOcafResource="$CASROOT"/share/opencascade/resources/XmlOcafResource
+export CSF_GraphicShr="$CASROOT"/lib/opencascade/libTKOpenGl.so
--- opencascade/opencascade.sh
+++ opencascade/opencascade.sh
@@ -1,2 +1,25 @@
-export CASROOT=/opt/opencascade
-source /opt/opencascade/env.sh
+#!/bin/sh -f
+
+export CASROOT=/usr
+
+export CSF_LANGUAGE=us
+export MMGT_CLEAR=1
+export CSF_EXCEPTION_PROMPT=1
+
+export CSF_SHMessage="$CASROOT"/share/opencascade/resources/SHMessage
+export CSF_MIGRATION_TYPES="$CASROOT"/share/opencascade/resources/StdResource/MigrationSheet.txt
+export CSF_MDTVTexturesDirectory="$CASROOT"/share/opencascade/resources/Textures
+export CSF_XSMessage="$CASROOT"/share/opencascade/resources/XSMessage
+export CSF_StandardDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_PluginDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_XCAFDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_StandardLiteDefaults="$CASROOT"/share/opencascade/resources/StdResource
+export CSF_UnitsLexicon="$CASROOT"/share/opencascade/resources/UnitsAPI/Lexi_Expr.dat
+export CSF_UnitsDefinition="$CASROOT"/share/opencascade/resources/UnitsAPI/Units.dat
+export CSF_IGESDefaults="$CASROOT"/share/opencascade/resources/XSTEPResource
+export CSF_STEPDefaults="$CASROOT"/share/opencascade/resources/XSTEPResource
+export CSF_XmlOcafResource="$CASROOT"/share/opencascade/resources/XmlOcafResource
+export CSF_GraphicShr="$CASROOT"/lib/opencascade/libTKOpenGl.so
+
+
+#source /opt/opencascade/env.sh
