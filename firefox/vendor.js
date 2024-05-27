b46e44d804f7ae688124e7a85a2fc4ff7149b3af
demmm <anke62@gmail.com>
2024-05-27 10:15:55 +0000

    appmenu-gtk-module 24.05 regular tar only contained 24.02, use full tar this time, build from submodule qbittorrent 4.6.5, patch obsolete firefox tested building 127 beta, same facebook issuenot uploaded, remove 1 obsolete vendor.js entry

--- firefox/vendor.js
+++ firefox/vendor.js
@@ -6,4 +6,3 @@ pref("browser.shell.checkDefaultBrowser", false);
 
 // Don't disable bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
-pref("extensions.shownSelectionUI", true);
